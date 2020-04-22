6e0737cbe16e1080c75e6c075ad7b320891ca602
GaryTsai1993 <jaylp33@gmail.com>
2020-04-22 15:14:51 +1000

    Pushing everythin

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'app', 'index'),
+  watch: true,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: "bundle.js",
+    chunkFilename: '[name].js'
+  },
+  module: {
+    rules: [{
+      test: /.jsx?$/,
+      include: [
+        path.resolve(__dirname, 'app')
+      ],
+      exclude: [
+        path.resolve(__dirname, 'node_modules')
+      ],
+      loader: 'babel-loader',
+      query: {
+        presets: [
+          ["@babel/env", {
+            "targets": {
+              "browsers": "last 2 chrome versions"
+            }
+          }]
+        ]
+      },
+    },
+  {
+    test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+  }]
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx']
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '/dist/'),
+    inline: true,
+    host: 'localhost',
+    port: 8080,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? 'style.css' : 'style.min.css',
+      chunkFilename: devMode ? 'style01.css' : '[style01.min.css',
+    }),
+  ]
+};
